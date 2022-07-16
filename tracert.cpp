3c4afc9ebd6ee9060bff5dc6f07fa86875bfb59a
pyvyx <vyx3327@web.de>
2022-07-16 15:05:43 +0200

    Ping packet creation on class construction && flush 'PrintRecv' func

--- tracert.cpp
+++ tracert.cpp
@@ -32,36 +32,12 @@
 #define RECV_TIMEOUT    1       // timeout delay for receiving packets in seconds
 
 
-// ping packet structure
-struct ping_pkt
-{
-    icmphdr hdr;
-    char msg[PING_PKT_S-sizeof(icmphdr)];
-};
- 
-
-// Calculating the Check Sum
-unsigned short checksum(void *b, int len)
-{    unsigned short *buf = static_cast<unsigned short*>(b);
-    unsigned int sum=0;
-    unsigned short result;
- 
-    for ( sum = 0; len > 1; len -= 2 )
-        sum += *buf++;
-    if ( len == 1 )
-        sum += *(unsigned char*)buf;
-    sum = (sum >> 16) + (sum & 0xFFFF);
-    sum += (sum >> 16);
-    result = ~sum;
-    return result;
-}
-
 
 namespace DNS
 {
     std::string Lookup(const std::string& address, sockaddr_in& addrCon)
     {
-        hostent* hostEntity;
+        const hostent* hostEntity;
         if((hostEntity = gethostbyname(address.c_str())) == nullptr) {
             std::cerr << "\nDNS lookup failed! Could not resolve hostname!\n";
             return std::string(); // No ip found for this hostname
@@ -121,24 +97,50 @@ namespace Time
 class Socket
 {
 private:
+    // ping packet structure
+    struct PingPkt
+    {
+        icmphdr hdr;
+        char msg[PING_PKT_S-sizeof(icmphdr)];
+    };
+private:
     sockaddr_in* m_AddrCon;
     const std::string& m_IpAddress;
-
+    
     int m_SocketFd = -1;
     size_t m_TTL = 1;
+    PingPkt m_PingPkt;
 private:
-    void PrintRecv(const std::string& ipAddPckRecv, double rtt)
+    void PrintRecv(const std::string& ipAddPckRecv, double rtt) const noexcept
     {
         // 15 == max length for ipv4 address
         size_t spaces = 15 - ipAddPckRecv.size();
         std::string buffer(spaces, ' ');
-        std::cout << m_TTL << ' ' << ipAddPckRecv << buffer << " ttl=" << m_TTL << " rtt=" << rtt << " ms" << '\n';
+        std::cout << m_TTL << ' ' << ipAddPckRecv << buffer << " ttl=" << m_TTL << " rtt=" << rtt << " ms" << std::endl;
     }
 
 
-    std::optional<std::string> Ping() noexcept
+    // Calculating the Check Sum
+    unsigned short Checksum(void *b, int len) const noexcept
+    {   
+        unsigned short *buf = static_cast<unsigned short*>(b);
+        unsigned int sum=0;
+        unsigned short result;
+     
+        for ( sum = 0; len > 1; len -= 2 )
+            sum += *buf++;
+        if ( len == 1 )
+            sum += *(unsigned char*)buf;
+        sum = (sum >> 16) + (sum & 0xFFFF);
+        sum += (sum >> 16);
+        result = ~sum;
+        return result;
+    }
+    
+
+    PingPkt GetPingPkt() const noexcept
     {
-        ping_pkt pckt;
+        PingPkt pckt;
         bzero(&pckt, sizeof(pckt)); // fill packet with 0 bytes
         
         pckt.hdr.type = ICMP_ECHO;
@@ -151,13 +153,19 @@ private:
 
         pckt.msg[i] = 0;
         pckt.hdr.un.echo.sequence = msg_count++;
-        pckt.hdr.checksum = checksum(&pckt, sizeof(pckt));
- 
+        pckt.hdr.checksum = Checksum(&pckt, sizeof(pckt));
+
+        return pckt;
+    }
+
+
+    std::optional<std::string> Ping() const noexcept
+    {
         Time::Sleep<std::chrono::microseconds>(PING_SLEEP_RATE);
  
         //send packet
         const Time::TimePoint timeStart = Time::GetCurrentTime();
-        if (sendto(m_SocketFd, &pckt, sizeof(pckt), MSG_WAITALL, (sockaddr*) m_AddrCon, sizeof(*m_AddrCon)) <= 0)
+        if (sendto(m_SocketFd, &m_PingPkt, sizeof(m_PingPkt), MSG_WAITALL, (sockaddr*) m_AddrCon, sizeof(*m_AddrCon)) <= 0)
         {
             std::cerr << "Packet Sending Failed! TTL: " << m_TTL << std::endl;
             return std::nullopt;
@@ -166,8 +174,9 @@ private:
         //receive packet
         sockaddr_in r_addr;
         int addr_len = sizeof(r_addr);
-        
-        if (recvfrom(m_SocketFd, &pckt, sizeof(pckt), MSG_WAITALL, (sockaddr*)&r_addr, (socklen_t*)&addr_len) <= 0)
+        PingPkt recvPkt;
+
+        if (recvfrom(m_SocketFd, &recvPkt, sizeof(recvPkt), MSG_WAITALL, (sockaddr*)&r_addr, (socklen_t*)&addr_len) <= 0)
         {
             //if(errno != EAGAIN)
             {
@@ -176,14 +185,14 @@ private:
             }
         }
 
-        const double rtt_msec = Time::DeltaTime<std::milli>(Time::GetCurrentTime(), timeStart);
+        const double rttTime = Time::DeltaTime<std::milli>(Time::GetCurrentTime(), timeStart);
         const std::string ipAddPckRecv = std::string(inet_ntoa(r_addr.sin_addr));
-        PrintRecv(ipAddPckRecv, rtt_msec);
+        PrintRecv(ipAddPckRecv, rttTime);
         return ipAddPckRecv;
     }
 public:
-    Socket(sockaddr_in* addrCon, const std::string& ipAddress) noexcept
-        : m_AddrCon(addrCon), m_IpAddress(ipAddress) {}
+    Socket(sockaddr_in* addrCon, const std::string& ipAddress) noexcept 
+        : m_AddrCon(addrCon), m_IpAddress(ipAddress), m_PingPkt(GetPingPkt()) {}
     ~Socket() noexcept 
     {
         if(close(m_SocketFd) != 0)
@@ -196,12 +205,13 @@ public:
         m_SocketFd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
         if(m_SocketFd < 0)
         {
-            std::cerr << "Failed to open socket! Make sure you run with root privileges\n";
+            std::cerr << "Failed to open socket! Make sure you are running with root privileges\n";
             return false;
         }
 
         int flags = fcntl(m_SocketFd, F_GETFL, 0);
-        if (flags == -1) {
+        if (flags == -1) 
+        {
             std::cerr << "Failed to receive socket flags!\n";
             return false;
         }
@@ -260,13 +270,14 @@ int main()
     const std::string reverseDNS = DNS::ReverseLookup(ipAddress);
     if(ipAddress.empty() || reverseDNS.empty()) return EXIT_FAILURE;
 
-    std::cout << "traceroute to '" << website << "' (" << ipAddress <<  ")" << " reverse DNS '" << reverseDNS << "', " << hops << " hops max, " << PING_PKT_S << " bytes packets\n\n";
+    std::cout << "traceroute to '" << website << "' (" << ipAddress <<  ")" << " reverse DNS '" << reverseDNS << "', " << hops << " hops max, " << PING_PKT_S << " bytes packets" << std::endl;
 
     Socket socket(&addrCon, ipAddress);
     if(!socket.Init())
         return EXIT_FAILURE;
 
     const double traceTime = socket.Trace(hops);
-    std::cout << "\nTraceing time:   " << traceTime << " ms\n";
-    std::cout << "Traceroute time: " << Time::DeltaTime<std::milli>(Time::GetCurrentTime(), startTime) << " ms" << std::endl;
+    const double endTime = Time::DeltaTime<std::milli>(Time::GetCurrentTime(), startTime);
+    std::cout << "\nTraceing time:   " << traceTime << " ms | " << (traceTime / 1000.0) << " sec\n";
+    std::cout << "Traceroute time: " << endTime << " ms | " << (endTime / 1000.0) << " sec" << std::endl;
 }
